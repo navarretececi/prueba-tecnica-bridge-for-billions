bbd6765bc6a6c348144471df6f5436173a281372
Cecilia Navarrete <navarretececi@gmail.com>
2023-03-27 12:52:21 +0200

    add disabled class and fix styles

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -43,12 +43,14 @@ const Pagination = () => {
   return (
     <div className="row">
       <Btn
-        onClick={firstPage ? () => handlerPagination(firstPage) : ""}
+        onClick={firstPage ? () => handlerPagination(firstPage) : () =>{}}
         btn_text={`<<`}
+        disabled={firstPage  ? "" : "disabled"}
       />
       <Btn
-        onClick={prevPage ? () => handlerPagination(prevPage) : ""}
+        onClick={prevPage ? () => handlerPagination(prevPage) : () =>{}}
         btn_text={`<`}
+        disabled={prevPage  ? "" : "disabled"}
       />
       {pagination.urls ? (
         <span>
@@ -56,12 +58,14 @@ const Pagination = () => {
         </span>
       ) : null}
       <Btn
-        onClick={nextPage ? () => handlerPagination(nextPage) : null}
+        onClick={nextPage ? () => handlerPagination(nextPage) : () =>{}}
         btn_text={`>`}
+        disabled={nextPage  ? "" : "disabled"}
       />
       <Btn
-        onClick={lastPage ? () => handlerPagination(lastPage) : ""}
+        onClick={lastPage ? () => handlerPagination(lastPage) : () =>{}}
         btn_text={`>>`}
+        disabled={lastPage  ? "" : "disabled"}
       />
     </div>
   );
--- src/components/bnt/Btn.js
+++ src/components/bnt/Btn.js
@@ -3,7 +3,7 @@ import "./Btn.css";
 const Btn = (props) => {
   return (
     <div className="container_btn">
-      <button id={props.id} className="btn" onClick={props.onClick}>
+      <button id={props.id} className="btn" onClick={props.onClick} disabled={props.disabled}>
         {props.btn_text}
       </button>
     </div>
