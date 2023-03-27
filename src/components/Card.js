ec2ff27edac2f5677ebd6c31d3c39e97e553dc93
Cecilia Navarrete <navarretececi@gmail.com>
2023-03-27 19:01:00 +0200

    add default img in card

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,6 +7,7 @@ const Card = (props) => {
 
   const handlerShowInfo = (id) => {
     let btn = document.getElementById(id);
+    
     if (btn.innerText === "More info") {
       btn.innerText = "Hide info";
     } else btn.innerText = "More info";
@@ -17,14 +18,8 @@ const Card = (props) => {
     <div className="card">
       <div className="row">
         <div className="row">
-          <Btn
-            btn_text="+"
-            onClick={() => console.log("agregado")}
-          />
-          <Btn
-            btn_text="-"
-            onClick={() => console.log("eliminado")}
-          />
+          <Btn btn_text="+" onClick={() => console.log("agregado")} />
+          <Btn btn_text="-" onClick={() => console.log("eliminado")} />
         </div>
         <Btn
           id={`btn_${props.id}`}
@@ -32,23 +27,55 @@ const Card = (props) => {
           onClick={() => handlerShowInfo(`btn_${props.id}`)}
         />
       </div>
-      
+
       <div className="principal_info">
-        <h5>{props.title}</h5>
-        {props.url ?<img className="img" src={props.url} alt="photo disk"></img> : null}
+        <h5 className="title">{props.title}</h5>
+        <img
+          className="img"
+          src={props.url ? props.url : "./music.svg"}
+          alt="photo disk"
+        />
       </div>
-      
+
       {showInfo ? (
         <div>
           {props.artist ? <h6>Artist: {props.artist}</h6> : null}
-          {props.format ? <h6>Format: {props.format.map((e) => {return e +" - ";})}</h6> : null}
-          {props.genre ? (<h6>Genre: {props.genre.map((e) => {return e +" - ";})}</h6>) : null}
-          {props.label ? (<h6>Label: {props.label.map((e) => {return e +" - ";})}</h6>) : null}
+          {props.format ? (
+            <h6>
+              Format:{" "}
+              {props.format.map((e) => {
+                return e + " - ";
+              })}
+            </h6>
+          ) : null}
+          {props.genre ? (
+            <h6>
+              Genre:{" "}
+              {props.genre.map((e) => {
+                return e + " - ";
+              })}
+            </h6>
+          ) : null}
+          {props.label ? (
+            <h6>
+              Label:{" "}
+              {props.label.map((e) => {
+                return e + " - ";
+              })}
+            </h6>
+          ) : null}
           {props.type ? <h6>Type: {props.type}</h6> : null}
           {props.trackinfo ? <h6>Trackinfo: {props.trackinfo}</h6> : null}
           {props.year ? <h6>Year: {props.year}</h6> : null}
           {props.country ? <h6>Country: {props.country}</h6> : null}
-          {props.style ? (<h6>Style: {props.style.map((e) => {return e +" - ";})}</h6>) : null}
+          {props.style ? (
+            <h6>
+              Style:{" "}
+              {props.style.map((e) => {
+                return e + " - ";
+              })}
+            </h6>
+          ) : null}
         </div>
       ) : null}
     </div>
