19200ee17edcd05d3dfb7805253a58dc3330b2cf
Cecilia Navarrete <navarretececi@gmail.com>
2023-03-29 12:39:18 +0200

    add services, show and add to collection

--- src/services.js
+++ src/services.js
@@ -1,7 +1,7 @@
 export const search = async (url) => {
   let response = await fetch(url, {
     headers: {
-      Authorization: "Discogs token=naLZrQwSiepVEgdldAJfdwVpLkSQCmxPGSUItYyq",
+      Authorization: `Discogs token=${process.env.REACT_APP_API_TOKEN}`,
     },
   })
     .then((response) => response.json())
@@ -11,4 +11,47 @@ export const search = async (url) => {
     .catch((error) => console.log(error));
 
   return response;
+};
+
+
+export const addToCollection = async (id) => {
+  const request = {
+    method: "POST",
+    headers: {
+      Authorization: `Discogs token=${process.env.REACT_APP_API_TOKEN}`,
+    },
+  };
+
+  let response = await fetch(
+    `https://api.discogs.com/users/navarretececi/collection/folders/1/releases/${id}`,
+    request
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => console.log(error));
+
+  return response;
+};
+
+
+export const showCollection = async () => {
+  const request = {
+    method: "GET",
+    headers: {
+      Authorization: `Discogs token=${process.env.REACT_APP_API_TOKEN}`,
+    },
+  };
+  let response = await fetch(
+    `https://api.discogs.com/users/navarretececi/collection/folders/1/releases?per_page=16`,
+    request
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return data
+    })
+    .catch((error) => console.log(error));
+
+    return response
 };
